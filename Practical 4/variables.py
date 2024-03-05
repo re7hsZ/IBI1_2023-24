7d0c45739433c1a8c334565974fe75560ca1065e
Yichen Zhu <156887382+CATbutNotCatalyzer@users.noreply.github.com>
2024-03-05 15:36:46 +0800

    Update variables.py

--- Practical 4/variables.py
+++ Practical 4/variables.py
@@ -1,1 +1,11 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+# e is greater than d. A month of running and strength training is had a greater improvement on running time.
 
+X=True
+Y=False
+W= X or Y
+# W is True
