e04d004972569b0441526109dfbad4a02ce30a81
Yichen Zhu <156887382+CATbutNotCatalyzer@users.noreply.github.com>
2024-03-05 16:19:01 +0800

    Update culture_density.py

--- Practical 4/culture_density.py
+++ Practical 4/culture_density.py
@@ -2,7 +2,7 @@
 holiday = 0
 density = 5
 # cell line doubles
-while density <= 90:
+while density <= 90/2:
     density *= 2
     holiday += 1
 print ("I can stay away from the lab for" , holiday , "days.")
