e9ded1ac16518d77d03310114b2b4ee129fbfb53
Yichen Zhu <156887382+CATbutNotCatalyzer@users.noreply.github.com>
2024-03-05 16:15:52 +0800

    Add files via upload

---
+++ Practical 4/culture_density.py
@@ -0,0 +1,8 @@
+#set the lenth of holiday and the original culture density
+holiday = 0
+density = 5
+# cell line doubles
+while density <= 90:
+    density *= 2
+    holiday += 1
+print ("I can stay away from the lab for" , holiday , "days.")
