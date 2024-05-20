38423dedc8eee65afbb3c0908c750c98942fa2ba
Yichen Zhu <156887382+CATbutNotCatalyzer@users.noreply.github.com>
2024-05-20 21:06:39 +0800

    Correct the wrong code for W and add the truth table.

--- Practical 4/variables.py
+++ Practical 4/variables.py
@@ -5,7 +5,16 @@ d=a-b
 e=b-c
 # e is greater than d. A month of running and strength training is had a greater improvement on running time.
 
-X=True
-Y=False
-W= X or Y
-# W is True
+X = True
+Y = False
+if X == Y:
+    W = False
+else:
+    W = True
+print(W)
+
+#the true table of W:
+#X=True, Y=True, W=False
+#X=True, Y=False, W=True
+#X=False, Y=True, W=True
+#X=False, Y=False, W=False
