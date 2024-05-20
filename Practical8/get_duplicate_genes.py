29ff346705674f10dc4710d61c3201fe39efdf5b
re7hsZ <161559598+re7hsZ@users.noreply.github.com>
2024-05-20 23:41:02 +0800

    Update get_duplicate_genes.py

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,29 +1,54 @@
-# open the folder contains the file
+# Pseudocode:
+# Change the current working directory to where the FASTA file is stored
+# Open the input FASTA file for reading and prepare to open an output file
+# Initialize variables to store sequence data and gene names
+# Loop through each line in the input file
+#   If the line starts with ">", it's a new gene, so process the previous gene's sequence
+#   Check if the description contains the word 'duplication'
+#   Write the gene information and sequence to the output file if it contains 'duplication'
+# After processing all genes, close the input and output files
+
+# Import necessary libraries
 import os
-os.chdir("/Users/zhuqin/Desktop/academic/IBI/Practical 8")
-# open input and output files
 import re
+
+# Change the current working directory to where the file is located
+os.chdir("C:\Users\lenovo\Desktop\IBI\IBI1_2023-24\IBI1_2023-24\Practical8")
+
+# Open the input FASTA file for reading and the output file for writing
 input_file = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
 output_file = open('duplicate_genes.fa', 'w')
-import regex # Import regex to solve the problem where the re cannot find fields that match the expression in overlapping areas
+
+# Initialize variables to store sequence data and gene names
 seq = ''
+gene_name = []
 i = 0
-name = []
 flag = 0
+
+# Loop through each line in the input file
 for line in input_file:
     if line.startswith(">"):
-        if flag == 1: # There is a problem that if the gene is in the first, seq will be empty. So I use a flag to review the first gene.
-            name[0] = name[0] + '\n' + seq + '\n'
-            output_file.write(name[0]) # write
-            flag = 0 
-        name.append(str(re.findall(r'gene:(.+)\sgene_biotype', line))) # see if it matches the regular expression
-        if re.search('duplication', line):
-            if i != 0:
-                name[i-1] = name[i-1] + '\n' + seq + '\n'
-                output_file.write(name[i-1]) # write
-            else:
-                flag = 1
-        i += 1
+        # Check if the current gene's sequence has been written to the output file
+        if flag == 1:
+            # Write the first gene's information and sequence to the output file
+            output_file.write(name[0] + '\n' + seq + '\n')
+            flag = 0
+        
+        # Extract the gene name and check if 'duplication' is in the description
+        description = line.strip().split()[1]
+        if 'duplication' in description:
+            # Write the gene's information and sequence to the output file
+            gene_name = re.findall(r'gene:(.+)\sgene_biotype', line)[0]
+            output_file.write(gene_name + '\n' + seq + '\n')
+        
+        # Reset the sequence string for the next gene and increment the gene counter
         seq = ''
+        i += 1
+        flag = 1  # Set the flag to indicate that a gene has been processed
     else:
-        seq += re.sub(r'\n','', line) # stroe the whole sequence without '\n' in the string
+        # Store the sequence data, removing newline characters
+        seq += re.sub(r'\n', '', line)
+
+# Close the input and output files
+input_file.close()
+output_file.close()
