52e8ff13658d0f7684112a594a7d078f0e7c8412
re7hsZ <161559598+re7hsZ@users.noreply.github.com>
2024-05-20 17:43:39 +0800

    Update variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,10 +1,29 @@
-a = 40
-b = 36
-c = 30
-d = 4
-e = 6
+# Initial time
+a = 40  # Initial 5km run time before any training
+b = 36  # Time after a month of running training
+c = 30  # Time after a second month of combined running and strength training
+
+# Calculate the time improvement
+d = a - b  # Time improvement from running training alone
+e = b - c  # Time improvement from combined running and strength training
+
+# Output the improvement results
+print("Improvement from running only:", d) # d = 4
+print("Improvement from running and strength training:", e) # e = 6
+
+# Comment explaining which training regime had a greater effect
+if e > d:
+    print("# Using a combination of running and strength exercises had a greater improvement on running time.")
+else:
+    print("# Running only had a greater improvement on running time.")
 #e is greater
 #Using a combination of running and strength exercises had a greater improvement on running time.
+# Boolean variables
+X = True
+Y = False
+
+# Dynamically calculate W based on the values of X and Y
+W = X or Y  # In Python, the 'or' operator represents the logical 'either X or Y'
 #W = 'True'
 #Outputs are like this
 '''
